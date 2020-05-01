913e3cc148b326838235e44fd31922a57c86376c
sazanca <moriya.kz817@gmail.com>
2020-05-01 19:24:56 +0900

    sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :name, presence: true, uniqueness:true 
-  #  空の列に対してエラーが発生するようにするため    
+  validates :name, presence: true, uniqueness: true       
 end
