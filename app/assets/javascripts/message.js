73e9842515f93ad202cdfe8c46516dbb5b37ec87
sazanca <moriya.kz817@gmail.com>
2020-05-06 12:25:20 +0900

    add message
    add message JSON HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,58 @@
-$(function(){
-
-  $("#new_message").on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-  });
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message">
+         <div class="message__upper__info">
+           <div class="message__upper__info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper__info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message__text">
+           <p class="message__text__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message">
+         <div class="message__upper__info">
+           <div class="message__upper__info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper__info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message__text">
+           <p class="message__text__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+  })
+})
 });
