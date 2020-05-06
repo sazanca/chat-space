687dd2ef854259ccb39c95f055a3ade49e8f6408
sazanca <moriya.kz817@gmail.com>
2020-05-06 12:49:32 +0900

    add message append
    add message append

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,19 +40,21 @@ $(function(){
    };
  }
 $('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-  })
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main_chat__messages').append(html);
+      $('form')[0].reset();
+    });
 })
 });
